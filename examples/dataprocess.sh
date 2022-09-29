2a61b82940a69de3a2a6c8aa537d35ee1b566046
d8K2E8uAnYK3oMCuLr <114600526+d8K2E8uAnYK3oMCuLr@users.noreply.github.com>
2022-09-29 17:45:01 +0800

    up

---
+++ examples/dataprocess.sh
@@ -0,0 +1,7 @@
+nohup python -u data_preprocess.py --dataset_name assist2009 >assist2009.log&
+nohup python -u data_preprocess.py --dataset_name algebra2005 >algebra2005.log&
+nohup python -u data_preprocess.py --dataset_name nips_task34 >nips_task34.log&
+nohup python -u data_preprocess.py --dataset_name statics2011 >statics2011.log&
+nohup python -u data_preprocess.py --dataset_name assist2015 >assist2015.log&
+nohup python -u data_preprocess.py --dataset_name poj >poj.log&
+nohup python -u data_preprocess.py --dataset_name bridge2algebra2006 >bridge2algebra2006.log&
